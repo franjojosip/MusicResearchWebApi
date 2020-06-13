5b7b3892472b746b4e55c60ca676f1ce52c4e90c
franjojosip <franjojosip.jukic2@gmail.com>
2020-06-13 22:51:17 +0200

    Generate new id on post

--- MusicResearchWebApi/Controllers/DatabaseController.cs
+++ MusicResearchWebApi/Controllers/DatabaseController.cs
@@ -202,7 +202,7 @@ namespace MusicResearchWebApi.Controllers
             return Request.CreateResponse(HttpStatusCode.OK);
         }
 
-        [HttpPost]
+        [HttpGet]
         [Route("", Name = "Song-Post")]
         public HttpResponseMessage PostSong([FromBody]Song model)
         {
@@ -210,7 +210,7 @@ namespace MusicResearchWebApi.Controllers
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string sql = $"INSERT INTO Song (Id, Name, GenreId, DateCreated) VALUES ('{model.Id}', '{model.Name}', {model.GenreId}, getdate())";
+                    string sql = $"INSERT INTO Song (Id, Name, GenreId, DateCreated) VALUES (newid(), '{model.Name}', {model.GenreId}, getdate())";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
                         connection.Open();
