4e0ba694cce254044e7a6107054a1408f726ff1f
franjojosip <franjojosip.jukic2@gmail.com>
2020-06-13 22:42:49 +0200

    Fix sql for insert song

--- MusicResearchWebApi/Controllers/DatabaseController.cs
+++ MusicResearchWebApi/Controllers/DatabaseController.cs
@@ -204,13 +204,13 @@ namespace MusicResearchWebApi.Controllers
 
         [HttpPost]
         [Route("", Name = "Song-Post")]
-        public HttpResponseMessage PostSong([FromUri]string name, int genreId)
+        public HttpResponseMessage PostSong([FromBody]Song model)
         {
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string sql = $"INSERT INTO Song VALUES (newid(), {name}, {genreId}, getdate())";
+                    string sql = $"INSERT INTO Song (Id, Name, GenreId, DateCreated) VALUES ({model.Id}, '{model.Name}', {model.GenreId}, getdate())";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
                         connection.Open();
